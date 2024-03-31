188f37ee3f9140e1e918608b51f9b94f5e5cd15e
bigbull666 <hylmar@seznam.cz>
2024-03-31 01:14:10 +0100

    First Joystic setup.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4233,9 +4233,9 @@
   //#define INVERT_JOY_Z  // Enable if Z direction is reversed
 
   // Use M119 with JOYSTICK_DEBUG to find reasonable values after connecting:
-  #define JOY_X_LIMITS { 5600, 8190-100, 8190+100, 10800 } // min, deadzone start, deadzone end, max
-  #define JOY_Y_LIMITS { 15000, 32768-2000, 32768+2000, 50536 }
-  #define JOY_Z_LIMITS { 4800, 8080-100, 8080+100, 11550 }
+  #define JOY_X_LIMITS { 5500-1000, 29500-2000, 29500+2000, 55000+2000 } // min, deadzone start, deadzone end, max
+  #define JOY_Y_LIMITS { 5500-1000, 29500-2000, 29500+2000, 55000+2000 }
+  #define JOY_Z_LIMITS { 5500-1000, 29500-2000, 29500+2000, 55000+2000}
   #define JOYSTICK_DEBUG
 #endif
 
